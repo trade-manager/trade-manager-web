49f1150e0ff818e0edd145b26c0dd95fcbeb4404
trade-manager <simon.allen@gmail.com>
2018-11-23 19:59:27 -0800

    Change table Manager to User

--- web/src/main/java/org/trade/web/DatabaseLoader.java
+++ web/src/main/java/org/trade/web/DatabaseLoader.java
@@ -10,44 +10,51 @@ import org.springframework.stereotype.Component;
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-	private final EmployeeRepository employees;
-	private final UserRepository users;
+    private final EmployeeRepository employees;
+    private final UserRepository users;
 
-	private final static String ROLE_MANAGER = "ROLE_MANAGER";
+    private final static String ROLE_MANAGER = "ROLE_MANAGER";
 
-	@Autowired
-	public DatabaseLoader(EmployeeRepository employeeRepository,
-						  UserRepository userRepository) {
+    @Autowired
+    public DatabaseLoader(EmployeeRepository employeeRepository,
+                          UserRepository userRepository) {
 
-		this.employees = employeeRepository;
-		this.users = userRepository;
-	}
+        this.employees = employeeRepository;
+        this.users = userRepository;
+    }
 
 
-	public void run(String... strings) throws Exception {
+    public void run(String... strings) throws Exception {
 
-		User greg = this.users.save(new User("admin", "admin",
-				ROLE_MANAGER));
-		User oliver = this.users.save(new User("oliver", "gierke",
-				ROLE_MANAGER));
+        User admin = this.users.findByName("admin");
+        if (null == admin) {
+            admin = this.users.save(new User("admin", "admin",
+                    ROLE_MANAGER));
+        }
+        User oliver = this.users.findByName("oliver");
+        if (null == oliver) {
+            oliver = this.users.save(new User("oliver", "admin",
+                    ROLE_MANAGER));
+        }
 
-		SecurityContextHolder.getContext().setAuthentication(
-			new UsernamePasswordAuthenticationToken("admin", "doesn't matter",
-				AuthorityUtils.createAuthorityList(ROLE_MANAGER)));
 
-		this.employees.save(new Employee("Frodo", "Baggins", "ring bearer", greg));
-		this.employees.save(new Employee("Bilbo", "Baggins", "burglar", greg));
-		this.employees.save(new Employee("Gandalf", "the Grey", "wizard", greg));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("admin", "doesn't matter",
+                        AuthorityUtils.createAuthorityList(ROLE_MANAGER)));
 
-		SecurityContextHolder.getContext().setAuthentication(
-			new UsernamePasswordAuthenticationToken("oliver", "doesn't matter",
-				AuthorityUtils.createAuthorityList(ROLE_MANAGER)));
+        this.employees.save(new Employee("Frodo", "Baggins", "ring bearer", admin));
+        this.employees.save(new Employee("Bilbo", "Baggins", "burglar", admin));
+        this.employees.save(new Employee("Gandalf", "the Grey", "wizard", admin));
 
-		this.employees.save(new Employee("Samwise", "Gamgee", "gardener", oliver));
-		this.employees.save(new Employee("Merry", "Brandybuck", "pony rider", oliver));
-		this.employees.save(new Employee("Peregrin", "Took", "pipe smoker", oliver));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("oliver", "doesn't matter",
+                        AuthorityUtils.createAuthorityList(ROLE_MANAGER)));
 
-		SecurityContextHolder.clearContext();
+        this.employees.save(new Employee("Samwise", "Gamgee", "gardener", oliver));
+        this.employees.save(new Employee("Merry", "Brandybuck", "pony rider", oliver));
+        this.employees.save(new Employee("Peregrin", "Took", "pipe smoker", oliver));
 
-	}
+        SecurityContextHolder.clearContext();
+
+    }
 }
