a32e7d826781bbe4b22c47528e7a333c9e285a5e
trade-manager <simon.allen@gmail.com>
2025-08-06 13:12:53 -0700

    MAINT: Add Rhino processing

--- strategies/org/trade/strategy/FiveMinGapBarStrategy.js
+++ strategies/org/trade/strategy/FiveMinGapBarStrategy.js
@@ -1,13 +1,15 @@
-function runStrategy(candleSeries, newBar) {
+function runStrategy(candleSeriesJSON, newBar) {
 
-  try {
-  let candleSeries = JSON.parse(candleSeries);
+    try {
+        let candleSeries = JSON.parse(candleSeriesJSON);
+        gs.log('Info: StrategyRuleJS::runStrategy key: ' + candleSeries.key + ' newBar: ' + newBar);
 
-
-  // Get the current candle
-  //CandleItem currentCandleItem = this.getCurrentCandle();
-  } catch (ex) {
-
-  }
+        // Get the current candle
+        let currentCandleItem = JSON.parse(gs.getCurrentCandleJSON());
+        gs.log('Info: StrategyRuleJS::runStrategy currentCandleItem: ' + currentCandleItem.candle.side);
+        return candleSeries.key
+    } catch (ex) {
+        gs.error(1, 100, 'Error: StrategyRuleJS::runStrategy process javascript msg: ' + ex.getMessage());
+    }
 }
 
