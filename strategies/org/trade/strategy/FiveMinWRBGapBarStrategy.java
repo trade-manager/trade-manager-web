fe5a3457b712ebeed3f0bd9535235b233a1e2639
trade-manager <simon.allen@gmail.com>
2025-10-12 14:54:50 -0700

    MAINT: Refactor repos

--- strategies/org/trade/strategy/FiveMinWRBGapBarStrategy.java
+++ strategies/org/trade/strategy/FiveMinWRBGapBarStrategy.java
@@ -2,10 +2,10 @@ package org.trade.strategy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.trade.core.aspect.Entrylimit;
 import org.trade.core.broker.IBrokerModel;
 import org.trade.core.persistent.TradeService;
 import org.trade.core.persistent.candle.Candle;
+import org.trade.core.persistent.codetype.EntryLimit;
 import org.trade.core.persistent.strategy.series.indicator.CandleSeries;
 import org.trade.core.persistent.strategy.series.indicator.StrategyData;
 import org.trade.core.persistent.strategy.series.indicator.candle.CandleItem;
